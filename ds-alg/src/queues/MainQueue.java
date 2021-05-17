3ca2c130d7f0f651f88e5a0e8ffcc8e2a6e4f5e6
Tomas Kaiser <tomas.kaiser06@gmail.com>
2021-05-17 09:50:44 +0200

    Add QueueReverser class

--- ds-alg/src/queues/MainQueue.java
+++ ds-alg/src/queues/MainQueue.java
@@ -6,15 +6,36 @@ import java.util.Stack;
 
 public class MainQueue {
     public static void main(String[] args){
-        StackQueue sq = new StackQueue();
-        sq.enqueue(10);
-        sq.enqueue(20);
-        sq.enqueue(30);
-        sq.dequeue();
-        sq.enqueue(40);
-        sq.dequeue();
-        sq.enqueue(50);
-        System.out.println(sq);
+
+        Queue<Integer> queue = new ArrayDeque<>();
+        queue.add(10);
+        queue.add(20);
+        queue.add(30);
+        queue.add(40);
+        queue.add(50);
+        QueueReverser qr = new QueueReverser();
+        qr.reverse(queue, 4);
+        System.out.println(queue);
+
+//        PriorityQueue pq = new PriorityQueue(5);
+//        pq.enqueue(4);
+//        pq.enqueue(5);
+//        pq.enqueue(2);
+//        pq.enqueue(1);
+//        pq.enqueue(3);
+//        pq.enqueue(6);
+//        System.out.println(pq);
+
+
+//        StackQueue sq = new StackQueue();
+//        sq.enqueue(10);
+//        sq.enqueue(20);
+//        sq.enqueue(30);
+//        sq.dequeue();
+//        sq.enqueue(40);
+//        sq.dequeue();
+//        sq.enqueue(50);
+//        System.out.println(sq);
 
 
 
@@ -24,7 +45,7 @@ public class MainQueue {
 //        aq.enqueue(30);
 //        aq.enqueue(40);
 //        aq.enqueue(50);
-////        aq.enqueue(50);
+//        aq.enqueue(50);
 //
 //        aq.dequeue();
 //        aq.dequeue();
--- ds-alg/src/queues/PriorityQueue.java
+++ ds-alg/src/queues/PriorityQueue.java
@@ -1,9 +1,63 @@
 package queues;
 
+import java.util.Arrays;
+
 public class PriorityQueue {
     // insert(2)
     // [1, 3, 5, 7]
     // items[i + 1] = items[i] X wrong approach
     // iterate from the back to the front to not loose the number
     // when shifting to the right
+
+    private int[] items;
+    private int count;
+
+    public PriorityQueue(int size) {
+        this.items = new int[size];
+    }
+
+    // [1, 3, 5, 0]
+    // insert 2
+//    1, 2, 4
+    public void enqueue(int item) {
+        if (isEmpty()) {
+            items[count++] = item;
+            return;
+        }
+
+        if (isFull()) throw new IllegalStateException("Queue is full!");
+
+        int index = shiftItemsToInsert(item);
+        items[index] = item;
+        count++;
+
+    }
+
+    private int shiftItemsToInsert(int item) {
+        int i;
+        for (i = count - 1; i >= 0; i--) {
+            if (items[i] > item) {
+                items[i + 1] = items[i];
+            } else {
+                break;
+            }
+        }
+        return i + 1;
+    }
+
+    private boolean isFull() {
+        return count == items.length;
+    }
+
+    private boolean isEmpty() {
+        return count == 0;
+    }
+
+    @Override
+    public String toString() {
+        return "PriorityQueue{" +
+                "items=" + Arrays.toString(items) +
+                ", count=" + count +
+                '}';
+    }
 }
